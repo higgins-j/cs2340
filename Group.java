71174b94699a6af6a3ceec7b1ff27db2c83bf18b
just1higg <just1higg@yahoo.com>
2017-05-25 16:26:27 -0400

    Delete Group.java

--- Group.java
+++
@@ -1,6 +0,0 @@
-public class Group {
-    public static void main(String[] args) {
-        System.out.println("Let's learn how to make branches.");
-        System.out.println("Let's make pull requests too!");
-    }
-}
